9eb4a41bca30b7f0486353443f261f2339faac06
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-09-15 23:14:43 +0900

    Init: 프로젝트 초기 설정

---
+++ Jeo-yo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jeo-yo
+//
+//  Created by 김수경 on 9/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jeo-yo/Jeo_yoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jeo_yoApp.swift
+//  Jeo-yo
+//
+//  Created by 김수경 on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Jeo_yoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
