383eb44bc2ab7675c4d0ea675b92d8084a4aea33
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-09-21 20:40:38 +0900

    Feat: API 네트워크 레이어 구축
    Moya의 targetType을 채택한 Router Pattern을 사용한 네트워크 레이어 구축

---
+++ Jeo-yo/Network/Enum/HeaderContents.swift
@@ -0,0 +1,14 @@
+//
+//  HeaderContents.swift
+//  Jeo-yo
+//
+//  Created by 김수경 on 9/20/24.
+//
+
+import Foundation
+
+enum HeaderContents: String {
+    case authorization = "Authorization"
+    case contentType = "Content-Type"
+    case json = "application/json"
+}
